3eed9a3c0cb052100f54f5b3a8a6e3a84c2b08ba
Pratik Kumar <93330691+hoodboypk@users.noreply.github.com>
2024-04-13 17:44:52 +0530

    Update index.js

--- Dice Challenge/index.js
+++ Dice Challenge/index.js
@@ -1,1 +1,25 @@
-var randomNumber1 = Math.floor(Math.random()*6) + 1;
+// for dice 1
+var randomNumber1 = Math.floor(Math.random()*6) + 1;    // 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";  // dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;  // images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+// for dice 2
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var rnadomImageSource = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", rnadomImageSource);
+
+// winning losing draw case
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins! 🤓";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🤓";
+}
+else {
+    document.querySelector("h1").innerHTML = "It's a draw! 🤓";
+}
